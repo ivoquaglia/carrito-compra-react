881d667b276101bbe8ee71f312960dd16b0e8dd9
ivoquaglia <ivoquaglia99@gmail.com>
2023-09-19 12:14:04 -0300

    arreglos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,14 +15,14 @@ export const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <NavLink to="/" className="navbar-brand" >Carrito</NavLink>
+                <NavLink to="/" className="navbar-brand" >Compras</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink to="/" className="nav-link active" aria-current="page" >Compras</NavLink>
+                            <NavLink to="/carrito" className="nav-link active" aria-current="page" >Carrito</NavLink>
                         </li>
                     </ul>
                     <NavLink to="/carrito" >
